df77cc5c23ca9691d8a2eef4aabd8be550b5fda5
山根康作 <yamanekousaku@K-book-Air.local>
2023-09-18 17:57:23 +0900

    1st commmit

--- keiba.py
+++ keiba.py
@@ -402,39 +402,31 @@ class HorseResults:
 
     @staticmethod
     def scrape(horse_id_list):
-        """
-        馬の過去成績データをスクレイピングする関数
-
-        Parameters:
-        ----------
-        horse_id_list : list
-            馬IDのリスト
-
-        Returns:
-        ----------
-        horse_results_df : pandas.DataFrame
-            全馬の過去成績データをまとめてDataFrame型にしたもの
-        """
-
-        #horse_idをkeyにしてDataFrame型を格納
         horse_results = {}
         for horse_id in (horse_id_list):
             time.sleep(1)
             try:
                 url = 'https://db.netkeiba.com/horse/' + horse_id
                 df = pd.read_html(url)[3]
-                #受賞歴がある馬の場合、3番目に受賞歴テーブルが来るため、4番目のデータを取得する
                 if df.columns[0]=='受賞歴':
                     df = pd.read_html(url)[4]
                 df.index = [horse_id] * len(df)
                 horse_results[horse_id] = df
             except IndexError:
+                st.error(f"IndexError occurred for horse_id: {horse_id}")
                 continue
             except Exception as e:
-                print(e)
+                st.error(f"An exception occurred: {e}")
                 break
-            except:
-                break
+
+        if not horse_results:
+            st.error("horse_results is empty")
+            return
+
+        horse_results_df = pd.concat([horse_results[key] for key in horse_results])
+
+        return horse_results_df
+
 
         #pd.DataFrame型にして一つのデータにまとめる
         horse_results_df = pd.concat([horse_results[key] for key in horse_results])
